bbcb558794023d7a9ca8aa6a102629f2a977e532
Jack <jack@jack.(none)>
2014-12-02 02:20:30 +0800

    增加登录

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
-    url(r'^create/$',views.ProductController().create,name="pro"),
+    url(r'^create/$',login_required(views.ProductController().create,login_url="/user/login/"),name="pro"),
     url(r'^index/$',login_required(views.ProductController().index,login_url="/user/login/"),name="index"),
     url(r'^show/(?P<id>\w+)/$',login_required(views.ProductController().show,login_url="/user/login/"),name="show"),
     url(r'^delete/(?P<id>\w+)/$',login_required(views.ProductController().show,login_url="/user/login/"),name="show"),
